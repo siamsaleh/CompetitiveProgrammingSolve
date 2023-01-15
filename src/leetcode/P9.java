6d9977cec37b59dcdfffd0a9515afde3243ed3f4
siamsaleh <siamsaleh9@gmail.com>
2023-01-15 09:46:21 +0600

    Problem 9

--- src/leetcode/P9.java
+++ src/leetcode/P9.java
@@ -4,9 +4,8 @@ import java.util.Arrays;
 
 public class P9 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome(10));
+        System.out.println(isPalindrome(10));//121,-121,10
     }
-
     public static boolean isPalindrome(int x) {
         if (x<0){
             return false;
@@ -20,7 +19,6 @@ public class P9 {
                 array[i] = string.charAt(i);
                 arrayTwo[j] = string.charAt(i);
             }
-
             return Arrays.equals(array, arrayTwo);
         }
     }

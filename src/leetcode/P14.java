2050f12939d4441649663874a0a5b3ce2cbcf82a
siamsaleh <siamsaleh9@gmail.com>
2023-09-07 04:37:05 +0600

    Test Competitive

---
+++ src/leetcode/P14.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class P14 {
+
+    public static void main(String[] args) {
+        //To Do
+    }
+    public String longestCommonPrefix(String[] strs) {
+
+        return null;
+    }
+    
+}
